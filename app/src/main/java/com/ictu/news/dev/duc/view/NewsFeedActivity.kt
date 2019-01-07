1cf9d44da4f7f74866353a14f1a222384d07b89b
mduccc <duc2521997@gmail.com>
2019-01-07 15:13:38 +0700

    Init

---
+++ app/src/main/java/com/ictu/news/dev/duc/view/NewsFeedActivity.kt
@@ -0,0 +1,13 @@
+package com.ictu.news.dev.duc.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ictu.news.R
+
+class NewsFeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_feed)
+    }
+}
---
+++ app/src/main/java/com/ictu/news/dev/kien/view/PostActivity.kt
@@ -0,0 +1,13 @@
+package com.ictu.news.dev.kien.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ictu.news.R
+
+class PostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}
